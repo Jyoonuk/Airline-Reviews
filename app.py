a7ad47d968469effb81b7cbc3168f26207e84d73
Jyoonuk <105832347+Jyoonuk@users.noreply.github.com>
2022-06-08 14:27:59 +0900

    add

--- app.py
+++ app.py
@@ -1,27 +1,31 @@
 import streamlit as st
 import numpy as np
+from streamlit_option_menu import option_menu
+import streamlit.components.v1 as html
+from  PIL import Image
+import cv2
+from st_aggrid import AgGrid
+import plotly.express as px
+import io 
 from app_home import run_home
 from app_search import run_search
 from app_data import run_data
 from app_chart import run_chart
 from app_ml import run_ml
 
-def message_cleaning(sentence) :
-  # 1. 구두점 제거
-  Test_punc_removed = [char for char in sentence if char not in string.punctuation ]
-  # 2. 각 글자들을 하나의 문자열로 합친다.
-  Test_punc_removed_join = ''.join(Test_punc_removed)
-  # 3. 문자열에 불용어가 포함되어있는지 확인해서, 불용어 제거한다.
-  Test_punc_removed_join_clean = [word for word in Test_punc_removed_join.split() if word.lower() not in my_stopwords ]
-  # 4. 결과로 남은 단어들만 리턴한다.
-  return Test_punc_removed_join_clean
+
 
 
 def main () :
 
 
     menu = ['Home','Search','Data','Chart','recommend']
-    choice = st.sidebar.selectbox('MENU',menu)
+    
+    with st.sidebar:
+        url1 = 'https://search.pstatic.net/common/?src=http%3A%2F%2Fpost.phinf.naver.net%2FMjAxODAxMDdfMTYw%2FMDAxNTE1MzM3MTM0NTQ4.Hdkww5vIE5h_mkKreEMdmb1zlPRnSfKp0jD_6fgRm-Ug.FXGePotff90kQGSnEQ3EVrOBZsrSk-7zrdQJMgFMPkYg.JPEG%2FIKi10gHmf5S6iK9S-verhxL5Jbh0.jpg&type=sc960_832'
+        st.image(url1)
+        choice = option_menu('MENU',menu)
+        
 
     if choice == menu[0] :
         run_home()
